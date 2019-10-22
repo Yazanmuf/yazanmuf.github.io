18cfce9841b89c5345d5a954df3053c071ded94c
Yazan Mufleh <ymufleh@gmail.com>
2019-10-22 18:50:01 -0400

    Updates

---
+++ precache-manifest.5859121645c95c9eafe7c0622b10c426.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "97d6bb5bab41d1adfd1e8fbf88d75939",
+    "url": "/youtube-app/index.html"
+  },
+  {
+    "revision": "2ad86ec8a3b53983390a",
+    "url": "/youtube-app/static/js/2.c943065e.chunk.js"
+  },
+  {
+    "revision": "8af0cf4fcbcfe58d8323",
+    "url": "/youtube-app/static/js/main.ae843b92.chunk.js"
+  },
+  {
+    "revision": "8aa35f490150d7afc0e4",
+    "url": "/youtube-app/static/js/runtime-main.d685239b.js"
+  }
+]);
